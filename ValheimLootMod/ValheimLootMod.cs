073cd3d6a71d23528f9ff4309160223b8c0f45e9
wheatley1239 <wickselm2005@gmail.com>
2023-03-28 21:17:11 +0100

    awake function

--- ValheimLootMod/ValheimLootMod.cs
+++ ValheimLootMod/ValheimLootMod.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
 
 namespace ValheimLootMod
 {
-    public class ValheimLootMod
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInProcess("valheim.exe")]
+    public class ValheimLootMod : BaseUnityPlugin
     {
+        // setting mod details
+        private const string modGUID = "wheatley1239.ValheimLootMultiplier";
+        private const string modName = "Valheim Loot Multiplier by wheatley1239";
+        private const string modVersion = "0.0.0.1";
+
+        // harmony
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        // unity program start
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
     }
 }
